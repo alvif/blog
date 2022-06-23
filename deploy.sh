2aa95b684b6cb9d5af971706dab15c40b624c427
AlvifSandana <asm240800@gmail.com>
2022-06-23 09:57:20 +0700

    update deploy.sh command

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 echo -e "\033[0;Deploying updates to Github...\033[0m"
-
 hugo
-
 cd public
-
+git checkout master
 git add -A 
-
 msg="rebuilding https://blog.alvifsandana.me/ - `date`"
 if [ $# -eq 1 ]
     then msg="$1"
 fi
 
 git commit -m "$msg"
-
 git push
 
 cd ..
+git add public/
+git commit -m "update submodule - `date`"
+git push
